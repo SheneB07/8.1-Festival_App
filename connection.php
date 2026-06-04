d030caa45ee83c597c24f304ce9fc8248aebb98d
SheneB07 <shenebanga@gmail.com>
2026-06-05 01:00:53 +0200

    live zetten + bug test

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "localhost";
-// $username = "u240660_ufestival"; 
-// $password = "kMMbQ7F5B3zZ4qNByQ5f";
-// $dbname = "u240660_ufestival";  // Add a semicolon here
-
 $servername = "localhost";
-$username = "root"; 
-$password = "";
-$dbname = "8.1_ufestival";  // Add a semicolon here
+$username = "u240660_ufestival"; 
+$password = "kMMbQ7F5B3zZ4qNByQ5f";
+$dbname = "u240660_ufestival";  // Add a semicolon here
+
+// $servername = "localhost";
+// $username = "root"; 
+// $password = "";
+// $dbname = "8.1_ufestival";  // Add a semicolon here
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);  // Use $dbname
